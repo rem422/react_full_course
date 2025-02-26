170109ada310f02eb2c4e6a5dbca2411f3b33cba
Rem Simiyu <remsimiyu@gmail.com>
2025-02-26 23:56:18 +0300

    Udemy React JS Full Course

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import '../src/App.css';
-import First_React_App from "./components/Getting_Started/First_React_App"
+import First_React_App from "./components/Section1_Getting_Started/First_React_App"
+import Component from "./components/Section3_React_Essentials/Components"
 
 const App = () => {
   return (
     <div>
         <First_React_App />
+        <Component />
     </div>
   )
 }
